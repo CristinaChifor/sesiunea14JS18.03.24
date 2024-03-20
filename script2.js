de02cb909d648b356184df96bb73ad0a9d0864c1
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-20 22:28:33 +0200

    Add DOM elements access

--- script2.js
+++ script2.js
@@ -1,1 +1,10 @@
 console.log('hello');
+
+// DOM este o reprezentare a documentului HTML într-o structură de tip arbore, 
+// unde fiecare element din document este reprezentat printr-un obiect (nod) în arborele DOM.
+
+// accesarea unui element după valoarea atributului id.
+
+const cardElement = document.getElementById('card');
+console.log('card: ', cardElement);
+console.log(`card: ${cardElement}`);
